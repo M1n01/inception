94f5e9b305f40e916c5be6a45b8af0bd2325af91
M1n01 <abe.minato.bz@gmail.com>
2024-04-03 21:34:04 +0900

    fix: wordpress

--- srcs/wordpress/tools/start.sh
+++ srcs/wordpress/tools/start.sh
@@ -5,10 +5,10 @@ while ! mysqladmin ping -hmariadb --silent; do
 done
 
 if [ ! -f "/var/www/html/wp-config.php" ]; then
-    wp core download --allow-root
-    wp config create --dbname=wordpress --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=mariadb --allow-root
-    wp core install --url=https://login.42.fr --title="Inception WordPress" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --allow-root
-    wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --user_pass=$WORDPRESS_USER_PASSWORD --role=author --allow-root
+    wp --allow-root core download
+    wp --allow-root config create --dbname=wordpress --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=mariadb
+    wp --allow-root core install --url=https://minabe.42.fr --title="Inception WordPress" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email
+    wp --allow-root user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --user_pass=$WORDPRESS_USER_PASSWORD --role=author
 fi
 
-exec php-fpm7 -F
+exec php-fpm81
