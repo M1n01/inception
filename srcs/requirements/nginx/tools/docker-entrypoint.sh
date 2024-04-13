62ad15ea396d78724592f466638fa98fe56627f5
M1n01 <abe.minato.bz@gmail.com>
2024-04-13 22:21:33 +0900

    fix: nginx docker-entrypoint.sh

--- srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -5,7 +5,7 @@ if [ ! -f "$CERT_DIR/$CERT_NAME.crt" ]; then
 
 	openssl genrsa -out $CERT_DIR/$CERT_NAME.key "$RSA_KEY_SIZE"
 	openssl req -new -key $CERT_DIR/$CERT_NAME.key -out $CERT_DIR/$CERT_NAME.csr -subj "$SUBJ"
-	openssl -x509 -key $CERT_DIR/$CERT_NAME.key -in $CERT_DIR/$CERT_NAME.csr -out $CERT_DIR/$CERT_NAME.crt -days 365
+	openssl x509 -key $CERT_DIR/$CERT_NAME.key -in $CERT_DIR/$CERT_NAME.csr -out $CERT_DIR/$CERT_NAME.crt -days 365
 fi
 
 exec nginx -g 'daemon off;'
