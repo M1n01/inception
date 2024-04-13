eecb021c18c5c678230b1ac05f27b0e305a2667e
M1n01 <abe.minato.bz@gmail.com>
2024-04-13 23:12:46 +0900

    tmp

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+if [ ! -d "${MYSQL_DATA_PATH}/mysql" ]; then
 	mysql_install_db
 	mysqld --bootstrap << EOF
 		FLUSH PRIVILEGES;
-		ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-		CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-		CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-		GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%';
+		ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+		CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+		CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+		GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%';
 EOF
 fi
 
-mysqld_safe
+exec mysqld --datadir=/var/lib/mysql --user=mysql
