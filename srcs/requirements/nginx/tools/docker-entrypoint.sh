f61cd8a562c45a6733c32da6533f0b82d5ed63e1
M1n01 <abe.minato.bz@gmail.com>
2024-04-14 17:07:28 +0900

    fix: ssl証明デバッグ

--- srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -4,8 +4,22 @@ if [ ! -f "$CERT_DIR/$CERT_NAME.crt" ]; then
 	SUBJ="/C=$COUNTRY/ST=$STATE/O=$ORGANIZATION/CN=$DOMAIN_NAME"
 
 	openssl genrsa -out $CERT_DIR/$CERT_NAME.key "$RSA_KEY_SIZE"
+	if [ $? -ne 0 ]; then
+        echo "RSA鍵の生成に失敗しました。"
+        exit 1
+    fi
+
 	openssl req -new -key $CERT_DIR/$CERT_NAME.key -out $CERT_DIR/$CERT_NAME.csr -subj "$SUBJ"
+	if [ $? -ne 0 ]; then
+        echo "CSRの生成に失敗しました。"
+        exit 1
+    fi
+
 	openssl x509 -req -days 365 -in $CERT_DIR/$CERT_NAME.csr -signkey $CERT_DIR/$CERT_NAME.key -out $CERT_DIR/$CERT_NAME.crt
+	if [ $? -ne 0 ]; then
+        echo "自己署名SSL証明書の生成に失敗しました。"
+        exit 1
+    fi
 fi
 
 exec nginx -g 'daemon off;'
