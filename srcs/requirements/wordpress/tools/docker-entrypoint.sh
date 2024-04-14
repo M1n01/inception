a0bcb0313855238f4380d57b12ca2dd6d7b3a2d1
M1n01 <abe.minato.bz@gmail.com>
2024-04-14 11:15:45 +0900

    fix: wp Dockerfile

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,37 +1,24 @@
 #!/bin/bash
 
-if ! wp core is-installed --allow-root --path=/var/www/html ; then
-	wp core download --allow-root --path=/var/www/html --locale=ja
- 
-	sleep 5
-	wp config create \
-		--allow-root \
-		--path=/var/www/html \
-		--locale=ja \
-		--dbname="$WP_DB_NAME" \
-		--dbuser="$WP_DB_USER" \
-		--dbpass="$WP_DB_PASSWORD" \
-		--dbhost="$WP_DB_HOST"
+wp config create \
+	--path=$WP_DATA_PATH \
+	--dbname=$DB_NAME \
+	--dbuser=$DB_USER \
+	--dbpass=$DB_PASSWORD \
+	--dbhost=$DB_HOST
+wp core install \
+	--path=$WP_DATA_PATH \
+	--url=$WP_URL \
+	--title="$WP_TITLE" \
+	--admin_user=$WP_ADMIN_USER \
+	--admin_password=$WP_ADMIN_PASSWORD \
+	--admin_email=$WP_ADMIN_EMAIL \
+	--skip-email
+wp user create \
+	$WP_EDITOR_USER \
+	$WP_EDITOR_EMAIL \
+	--path=$WP_DATA_PATH \
+	--role=author \
+	--user_pass=$WP_EDITOR_PASSWORD
 
-	sleep 3
-	wp core install \
-		--allow-root \
-		--path=/var/www/html \
-		--locale=ja \
-		--url="https://$DOMAIN" \
-		--title="$WP_TITLE" \
-		--admin_user="$WP_ADMIN_USER" \
-		--admin_password="$WP_ADMIN_PASSWORD" \
-		--admin_email="$WP_ADMIN_EMAIL"
-
-	wp user create \
-		--allow-root \
-		--path=/var/www/html \
-		"$WP_USER" \
-		"$WP_USER_EMAIL" \
-		--user_pass="$WP_USER_PASSWORD" \
-		--role=author
-fi
-
-# フォアグラウンドで待機する
-php-fpm7.4 -F
+exec php-fpm7.4 --nodaemonize
