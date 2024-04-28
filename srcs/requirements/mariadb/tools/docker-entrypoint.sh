439e0cfa52c4f1099d43b634ca800bb03f91391e
M1n01 <abe.minato.bz@gmail.com>
2024-04-28 15:19:14 +0900

    change: tabをspaceへ

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ ! -d "/var/lib/mysql/mariadb" ]; then 
-	mysql_install_db --defaults-file=/etc/mysql/mariadb.conf.d/50-server.cnf
-	mysqld --bootstrap << EOF
-		FLUSH PRIVILEGES;
-		ALTER USER 'root'@'localhost' IDENTIFIED BY 'root_password';
-		CREATE DATABASE IF NOT EXISTS mariadb;
-		CREATE USER 'wordpress'@'%' IDENTIFIED BY 'password';
-		GRANT ALL PRIVILEGES ON *.* TO 'wordpress'@'%';
+if [ ! -d "/var/lib/mysql/mariadb" ]; then
+    mysql_install_db --defaults-file=/etc/mysql/mariadb.conf.d/50-server.cnf
+    mysqld --bootstrap << EOF
+        FLUSH PRIVILEGES;
+        ALTER USER 'root'@'localhost' IDENTIFIED BY 'root_password';
+        CREATE DATABASE IF NOT EXISTS mariadb;
+        CREATE USER 'wordpress'@'%' IDENTIFIED BY 'password';
+        GRANT ALL PRIVILEGES ON *.* TO 'wordpress'@'%';
 EOF
 fi
 
--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 if ! wp core is-installed --allow-root --path=/var/www/html ; then
-	wp core download --allow-root --path=/var/www/html --locale=ja
- 
-	sleep 5
-	wp config create \
-		--allow-root \
-		--path=/var/www/html \
-		--locale=ja \
-		--dbname="wordpress" \
-		--dbuser="admin" \
-		--dbpass="password" \
-		--dbhost="mariadb"
+    wp core download --allow-root --path=/var/www/html --locale=ja
 
-	sleep 3
-	wp core install \
-		--allow-root \
-		--path=/var/www/html \
-		--locale=ja \
-		--url="https://localhost" \
-		--title="inception" \
-		--admin_user="supervisor" \
-		--admin_email="supervisor@example.com" \
-		--admin_password="abcd1234"
+    sleep 5
+    wp config create \
+        --allow-root \
+        --path=/var/www/html \
+        --locale=ja \
+        --dbname="wordpress" \
+        --dbuser="admin" \
+        --dbpass="password" \
+        --dbhost="mariadb"
 
-	wp user create \
-		--allow-root \
-		--path=/var/www/html \
-		"user" \
-		"user@sample.com" \
-		--user_pass="abcd1234" \
-		--role=author
+    sleep 3
+    wp core install \
+        --allow-root \
+        --path=/var/www/html \
+        --locale=ja \
+        --url="https://localhost" \
+        --title="inception" \
+        --admin_user="supervisor" \
+        --admin_email="supervisor@example.com" \
+        --admin_password="abcd1234"
+
+    wp user create \
+        --allow-root \
+        --path=/var/www/html \
+        "user" \
+        "user@sample.com" \
+        --user_pass="abcd1234" \
+        --role=author
 fi
 
 # フォアグラウンドで待機する
