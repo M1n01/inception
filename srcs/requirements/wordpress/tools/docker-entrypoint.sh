230772cd692ae4f96e4c8972bc6630afee12514a
M1n01 <abe.minato.bz@gmail.com>
2024-06-08 19:41:53 +0900

    fix: wp entrypoint.sh syntax

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -9,7 +9,7 @@ if ! su -s /bin/bash www-data -c "wp core is-installed --path=/var/www/html"; th
     su -s /bin/bash www-data -c "wp config create --path=/var/www/html --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=mariadb"
 
     echo "Installing WordPress..."
-    su -s /bin/bash www-data -c "wp core install --path=/var/www/html --url=${WP_URL} --title='${WP_TITLE}' --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}"
+    su -s /bin/bash www-data -c "wp core install --path=/var/www/html --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}"
 
     echo "Creating Editor User..."
     su -s /bin/bash www-data -c "wp user create --path=/var/www/html ${WP_EDITOR_USER} ${WP_EDITOR_EMAIL} --user_pass=${WP_EDITOR_PASSWORD} --role=editor"
