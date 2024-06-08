c05570bca73ed4ee2bb158267250f9d71fe323da
M1n01 <abe.minato.bz@gmail.com>
2024-06-08 17:54:21 +0900

    fix: wp entrypoint.sh

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,36 +1,40 @@
 #!/bin/bash
-if ! wp core is-installed --allow-root --path=/var/www/html ; then
 
-    wp core download --allow-root --path=/var/www/html --locale=ja
+# Check if wp-config.php exists, if not create it and install WordPress
+if ! su -s /bin/bash www-data -c "wp core is-installed --path=/var/www/html"; then
+    echo "Downloading WordPress core files..."
+    su -s /bin/bash www-data -c \
+        "wp core download --path=/var/www/html --locale=ja"
 
-    echo "...Create wp-config.php"
+    echo "Creating wp-config.php..."
+    su -s /bin/bash www-data -c \
+        "wp config create \
+            --path=/var/www/html \
+            --locale=ja \
+            --dbname=$DB_NAME \
+            --dbuser=$DB_USER \
+            --dbpass=$DB_PASSWORD \
+            --dbhost='mariadb'"
 
-    wp config create \
-        --path=/var/www/html \
-        --locale=ja \
-        --dbname=${DB_NAME} \
-        --dbuser=${DB_USER} \
-        --dbpass=${DB_PASSWORD} \
-        --dbhost="mariadb"
+    echo "Installing WordPress..."
+    su -s /bin/bash www-data -c \
+        "wp core install \
+            --path=/var/www/html \
+            --url=$WP_URL \
+            --title='$WP_TITLE' \
+            --admin_user=$WP_ADMIN_USER \
+            --admin_password=$WP_ADMIN_PASSWORD \
+            --admin_email=$WP_ADMIN_EMAIL"
 
-    echo "...Install WordPress"
-
-    wp core install \
-        --path=/var/www/html \
-        --url=$WP_URL \
-        --title="$WP_TITLE" \
-        --admin_user=$WP_ADMIN_USER \
-        --admin_password=$WP_ADMIN_PASSWORD \
-        --admin_email=$WP_ADMIN_EMAIL
-
-    echo "...Create Editor User"
-
-    wp user create \
-        --path=/var/www/html \
-        "$WP_EDITOR_USER" \
-        "$WP_EDITOR_EMAIL" \
-        --user_pass=${WP_EDITOR_PASSWORD} \
-        --role=editor
+    echo "Creating Editor User..."
+    su -s /bin/bash www-data -c \
+        "wp user create \
+            --path=/var/www/html \
+            '$WP_EDITOR_USER' \
+            '$WP_EDITOR_EMAIL' \
+            --user_pass=${WP_EDITOR_PASSWORD} \
+            --role=editor"
 fi
 
+# Start php-fpm
 php-fpm7.4 -F
