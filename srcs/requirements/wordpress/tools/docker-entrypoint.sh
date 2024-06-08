932a40ad09aab8b992d4c8830977a233908b594c
M1n01 <abe.minato.bz@gmail.com>
2024-06-08 20:12:50 +0900

    fix: 結局allow-root

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -3,18 +3,18 @@
 chown -R www-data:www-data /run/php /var/www/html /var/www/.wp-cli
 
 # Check if wp-config.php exists, if not create it and install WordPress
-if ! su -s /bin/bash www-data -c "wp core is-installed --path=/var/www/html"; then
+if ! wp core is-installed --allow-root --path=/var/www/html; then
     echo "Downloading WordPress core files..."
-    su -s /bin/bash www-data -c "wp core download --path=/var/www/html --locale=ja"
+    wp core download --allow-root --path=/var/www/html --locale=ja
 
     echo "Creating wp-config.php..."
-    su -s /bin/bash www-data -c "wp config create --path=/var/www/html --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb"
+    wp config create --allow-root --path=/var/www/html --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb
 
     echo "Installing WordPress..."
-    su -s /bin/bash www-data -c "wp core install --path=/var/www/html --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL"
+    wp core install --allow-root --path=/var/www/html --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
 
     echo "Creating Editor User..."
-    su -s /bin/bash www-data -c "wp user create --path=/var/www/html $WP_EDITOR_USER $WP_EDITOR_EMAIL --user_pass=$WP_EDITOR_PASSWORD --role=editor"
+    wp user create --allow-root --path=/var/www/html $WP_EDITOR_USER $WP_EDITOR_EMAIL --user_pass=$WP_EDITOR_PASSWORD --role=author
 fi
 
 # Start php-fpm
