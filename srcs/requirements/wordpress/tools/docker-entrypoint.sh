d444af26748ceafeaf0b776f2551d7259d54c94c
M1n01 <abe.minato.bz@gmail.com>
2024-06-09 10:24:16 +0900

    fix: exec wp entrypoint.sh by sudo

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -9,13 +9,13 @@ if ! wp core is-installed --allow-root --path=/var/www/html; then
     sudo -u www-data -- wp core download --path=/var/www/html --locale=ja
 
     echo "Creating wp-config.php..."
-    wp config create --path=/var/www/html --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb
+    sudo -u www-data -- wp config create --path=/var/www/html --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb
 
     echo "Installing WordPress..."
-    wp core install --path=/var/www/html --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+    sudo -u www-data -- wp core install --path=/var/www/html --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
 
     echo "Creating Editor User..."
-    wp user create $WP_EDITOR_USER $WP_EDITOR_EMAIL --path=/var/www/html --user_pass=$WP_EDITOR_PASSWORD --role=author
+    sudo -u www-data -- wp user create $WP_EDITOR_USER $WP_EDITOR_EMAIL --path=/var/www/html --user_pass=$WP_EDITOR_PASSWORD --role=author
 fi
 
 # Start php-fpm
