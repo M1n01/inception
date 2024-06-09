9252eb64d0bcd6ee46b91dd488932b2455ecb8b9
M1n01 <abe.minato.bz@gmail.com>
2024-06-09 10:58:01 +0900

    allow-root

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -12,7 +12,7 @@ if ! wp core is-installed --allow-root --path=/var/www/html; then
     sudo -u www-data -- wp config create --path=/var/www/html --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb
 
     echo "Installing WordPress..."
-    sudo -u www-data -i -- wp core install --path=/var/www/html --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+    wp core install --path=/var/www/html --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
 
     echo "Creating Editor User..."
     sudo -u www-data -- wp user create $WP_EDITOR_USER $WP_EDITOR_EMAIL --path=/var/www/html --user_pass=$WP_EDITOR_PASSWORD --role=author
